4bd1da41034c2945c3dad7a3d18b4186d5dac537
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-04-24 21:53:44 +0300

    Configure for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'export', // Enable static HTML export
+  distDir: 'out',   // Directory GitHub Pages will serve from
   images: {
     unoptimized: true, // ✅ This fixes the error
   },
+  basePath: '/rednose-site', // Replace with your repo name
+  assetPrefix: '/rednose-site/', // Same as basePath
 };
 
 export default nextConfig;
